0466b9d4e0c4060e2a492c2cf0edce216a3a8a42
AnnabellAnastasia <oav2008@gmail.com>
2019-05-30 23:08:20 -0400

    Building content with JSX

--- src/index.js
+++ src/index.js
@@ -1,16 +1,29 @@
 // Import the React and ReactDOM libreries
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 
-// Creare a react componentt
-const App = ()=>{
-    return <div>
-        Hi there!
-    </div>
+
+
+// Creare a react component
+const App = () => {
+    const buttonText = { text: 'Click me!' }
+    const labelText = 'Enter name:'
+
+
+    return (
+        <div>
+            <label className="label" htmlFor="name">
+                {labelText}            
+                </label>
+            <input id="name" type="text" />
+            <button style={{ backgroundColor: 'blue', color: 'white' }}>
+                {buttonText.text}
+            </button>
+        </div>
+    );
+
+
 };
 
 // Take the react component and show it on the screen
-ReactDOM.render(
-   <App /> ,
-   document.querySelector('#root')
-);
+ReactDOM.render(<App />, document.querySelector("#root"));
